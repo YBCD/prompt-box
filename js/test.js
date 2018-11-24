996fde56b82c893c3fcf499e2bd5573f84e2ee72
ubang_jia <273242031@qq.com>
2018-11-24 14:39:31 +0800

    second

--- js/test.js
+++ js/test.js
@@ -3,9 +3,11 @@ $(function(){
     $("#allBack").append('<div id="cover"></div>'
                         +'<div id="box">'
                         +'弹框'+'<button id="xB" type="button">关闭</button>'
+                        +'<button id="quickPionterBu">快速指派</button>'
+                        +'<button id="cancelBu">取消</button>'
                         +'</div>'
     );
-    $("#xB").bind('click',function() {
+    $("#cancelBu").bind('click',function() {
       $("#cover").remove();
       $("#box").remove();
     });
